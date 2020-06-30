ceee91273a93f27ffc8fabd3b422494cdd9d820c
ramzanthenolph <ramzanthenolph@gmail.com>
2020-06-30 16:44:52 +0300

    add while loop to class and user inputs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,33 @@
-import java.io.Console;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
-        
+        Scanner input = new Scanner(System.in);
+        boolean menu = true;
+        System.out.println("********************** CAESAR CIPHER **********************\n" + "Welcome to Caesar Cipher,  Where you can Encrypt & Decrypt information\n");
+
+        while(menu){
+            System.out.println("What do you want to do? Input a number\n" + "1: Encrypt\n" + "2: Decrypt\n" + "3: Exit");
+            int menuSelect = input.nextInt();
+            input.nextLine();
+
+            if(menuSelect ==1){
+                System.out.println("Enter your plain text to be encrypted");
+                String userInput = input.nextLine();
+                System.out.println("In which direction do you want to encrypt your information? Right or Left. ");
+                String direction = "";
+
+                boolean validDirection = true;
+                while(validDirection){
+                    direction = input.nextLine();
+                    if(!direction.equalsIgnoreCase("right") && !direction.equalsIgnoreCase("left")){
+                        System.out.println("Please select a direction by typing left or right");
+                    }
+                    else {
+                        validDirection = false;
+                    }
+                }
+            }
+        }
     }
 }
